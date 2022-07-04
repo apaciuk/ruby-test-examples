18444a497767dbb647ece00a5d1ef54de93c303f
Paul Anthony McGowan <xhostcom@gmail.com>
2022-07-04 09:25:42 +0100

    Array tests

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -15,12 +15,12 @@ end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
-  if number 
-  number = array[-3..-1] 
+  if array
+  array = array[-3..-1] 
   else 
-  number = array[-1..-1]
+  array = array[-1..-1]
   end
-
+end
 def add_element(array)
   # add an element (of any value) to the array
   array = array.push("nala cat")
@@ -36,17 +36,32 @@ end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
+  array = array.drop(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
 
 def array_concatenation(original, additional)
   # return an array adding the original and additional array together
+   
+  a = [1, 3, 5] 
+  b = [2, 4, 6]
+  a.concat(b) 
+  a
+  
+  c = ["a", "b", "c"] 
+  d = [1, 2, 3] 
+  c.concat(d) 
+  c
+
 end
 
 def array_difference(original, comparison)
   # return an array of elements from the original array that are not in the comparison array
+  a = [0, 1, 1, 2, 3, 5]
+  b = [0, 1, 2]
+  a.delete_if { |x| b.include?(x) } 
+  a
 end
 
 def empty_array?(array)
